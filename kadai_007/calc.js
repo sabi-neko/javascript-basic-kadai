7d2a0b629c286a4402a29a3d5c33ff488540f4e3
sabineko <103361140+sabi-neko@users.noreply.github.com>
2023-11-09 07:09:10 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+//numは任意の数字90とする
+let num=90;
+
+
+if (num % 3===0 && num % 5===0){
+  console.log('3と5の倍数です');
+}
+else if (num % 3 ===0) {
+  console.log('3の倍数です');  
+}
+else if (num % 5 ===0){
+  console.log('5の倍数です');
+}
+else  {
+  console.log(num);
+}
